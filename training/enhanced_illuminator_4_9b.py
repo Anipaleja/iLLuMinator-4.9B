f5edd01791cbee2dd7d31d04a88dbcedbda4bf87
Rayhan Jamasi <rayhanjamasi@gmail.com>
2025-08-23 11:26:07 -0400

    Update enhanced_illuminator_4_9b.py

--- training/enhanced_illuminator_4_9b.py
+++ training/enhanced_illuminator_4_9b.py
@@ -200,6 +200,7 @@ class iLLuMinator4_9B(nn.Module):
         super().__init__()
         
         # Store configuration
+        self.gradient_cp = gradient_cp
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.max_seq_length = max_seq_length
